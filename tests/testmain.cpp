2552869495aa5024f7bc8dd4b8749e77e152ca7f
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-13 20:56:46 +0500

    Add more sha256 tests

--- tests/testmain.cpp
+++ tests/testmain.cpp
@@ -18,10 +18,25 @@ template <typename T> std::string HexStr(const T itbegin, const T itend)
 TEST(sha256Test, sha256)
 {
     std::string s = "hello";
+    std::string s1 = "019283109238ksla;jdxcv0z98cv012;lkk;asdjfkjxcv08091823091283kljvl;kxcj";
+    std::string s2 = "--123-0909-0123*(*";
+    std::string s3 = "@#)*()(*)!(@*0";
     std::vector<uint8_t> out(SHA256_RAW_BYTES_LENGTH);
     sha256(reinterpret_cast<const uint8_t*>(s.c_str()), s.length(), &out[0]);
     std::string expected = HexStr(out.begin(), out.end());
     EXPECT_EQ(expected, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824");
+
+    sha256(reinterpret_cast<const uint8_t*>(s1.c_str()), s1.length(), &out[0]);
+    expected = HexStr(out.begin(), out.end());
+    EXPECT_EQ(expected, "72465a5cb8c6818bc0687921da19efac30f9e12e1c7d6f8c69b91cc236958105");
+
+    sha256(reinterpret_cast<const uint8_t*>(s2.c_str()), s2.length(), &out[0]);
+    expected = HexStr(out.begin(), out.end());
+    EXPECT_EQ(expected, "5fc0f1b7b069aaca7137fd6c9980cb4162cc32f34c3ef3960b9b097940646537");
+
+    sha256(reinterpret_cast<const uint8_t*>(s3.c_str()), s3.length(), &out[0]);
+    expected = HexStr(out.begin(), out.end());
+    EXPECT_EQ(expected, "8d93a9972afbd63f943826fa6b1ec0e04e9526c7a168abeda69af63fa7abee18");
 }
 
 
