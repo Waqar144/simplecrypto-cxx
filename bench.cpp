8814609815c7cd5d8ee6c2fd352d86a6fa845883
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-13 23:22:37 +0500

    SHA 512 benchmark

--- bench.cpp
+++ bench.cpp
@@ -1,16 +1,26 @@
 #include <benchmark/benchmark.h>
 
 #include "sha256.h"
+#include "sha512.h"
 
 static void BM_SHA256(benchmark::State& state)
 {
     std::string s = "hello";
-    std::vector<uint8_t> out(32);
+    std::vector<uint8_t> out(SHA256_RAW_BYTES_LENGTH);
     for (auto _ : state)
         sha256(reinterpret_cast<const uint8_t*>(s.c_str()), s.length(), &out[0]);
 }
 
+static void BM_SHA512(benchmark::State& state)
+{
+    std::string s = "hello";
+    std::vector<uint8_t> out(SHA512_RAW_BYTES_LENGTH);
+    for (auto _ : state)
+        sha512(reinterpret_cast<const uint8_t*>(s.c_str()), s.length(), &out[0]);
+}
+
 
 BENCHMARK(BM_SHA256);
+BENCHMARK(BM_SHA512);
 
 BENCHMARK_MAIN();
