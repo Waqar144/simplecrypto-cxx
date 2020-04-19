e9c688a39c521e2bd4028870ac5da3d7862436d0
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-19 18:46:01 +0500

    Fix submodules

--- tests/testmain.cpp
+++ tests/testmain.cpp
@@ -11,7 +11,7 @@ template <typename T> std::string HexStr(const T itbegin, const T itend)
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     rv.reserve(std::distance(itbegin, itend) * 2);
     for (T it = itbegin; it < itend; ++it) {
-        unsigned char val = (unsigned char)(*it);
+        unsigned char val = static_cast<unsigned char>(*it);
         rv.push_back(hexmap[val >> 4]);
         rv.push_back(hexmap[val & 15]);
     }
