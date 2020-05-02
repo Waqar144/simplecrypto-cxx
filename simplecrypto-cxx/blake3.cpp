abd16076c3cb94be2b2eb4ed9089539225f71250
Waqar Ahmed <waqar.17a@gmail.com>
2020-05-02 22:49:27 +0500

    Add Blake3 interface

--- simplecrypto-cxx/blake3.cpp
+++ simplecrypto-cxx/blake3.cpp
@@ -1,6 +1,13 @@
 #include "blake3.h"
 
-blake3::blake3()
+template <>
+void hashBlake3(const std::string& in, std::vector<uint8_t>& out)
 {
+    blake3_hasher hasher;
+    blake3_hasher_init(&hasher);
+    blake3_hasher_update(&hasher, (const uint8_t*)in.data(), in.size());
 
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
 }
--- simplecrypto-cxx/blake3.h
+++ simplecrypto-cxx/blake3.h
@@ -1,11 +1,26 @@
-#ifndef BLAKE3_H
-#define BLAKE3_H
+#ifndef BLAKE3_CXX_H
+#define BLAKE3_CXX_H
 
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
 
-class blake3
+#include "blake3/blake3.h"
+
+template <typename Container>
+void hashBlake3(const Container& in, std::vector<uint8_t>& out)
 {
-public:
-    blake3();
-};
+    blake3_hasher hasher;
+    blake3_hasher_init(&hasher);
+    blake3_hasher_update(&hasher, in.data(), in.size());
 
-#endif // BLAKE3_H
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
+}
+
+template <>
+void hashBlake3(const std::string& in, std::vector<uint8_t>& out);
+
+#endif    // BLAKE3_H
