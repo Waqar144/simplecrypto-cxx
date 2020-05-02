b54be039c09aa1c4679b88dcd5b5481a6f762317
Waqar Ahmed <waqar.17a@gmail.com>
2020-05-03 01:19:19 +0500

    blake3 keyed and derive_key

--- simplecrypto-cxx/blake3.cpp
+++ simplecrypto-cxx/blake3.cpp
@@ -11,3 +11,27 @@ void hashBlake3(const std::string& in, std::vector<uint8_t>& out)
         out.resize(BLAKE3_OUT_LEN);
     blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
 }
+
+template <>
+void hashBlake3_keyed(const std::string& data, const std::string& key, std::vector<uint8_t>& out)
+{
+    blake3_hasher hasher;
+    blake3_hasher_init_keyed(&hasher, (const uint8_t*)key.data());
+    blake3_hasher_update(&hasher, (const uint8_t*)data.data(), data.size());
+
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
+}
+
+template <>
+void hashBlake3_deriveKey(const std::string& data, const std::string& context, std::vector<uint8_t>& out)
+{
+    blake3_hasher hasher;
+    blake3_hasher_init_derive_key(&hasher, context.data());
+    blake3_hasher_update(&hasher, (const uint8_t*)data.data(), data.size());
+
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
+}
--- simplecrypto-cxx/blake3.h
+++ simplecrypto-cxx/blake3.h
@@ -23,4 +23,34 @@ void hashBlake3(const Container& in, std::vector<uint8_t>& out)
 template <>
 void hashBlake3(const std::string& in, std::vector<uint8_t>& out);
 
+template <typename Container>
+void hashBlake3_keyed(const Container& data, const Container& key, std::vector<uint8_t>& out)
+{
+    blake3_hasher hasher;
+    blake3_hasher_init_keyed(&hasher, key.data());
+    blake3_hasher_update(&hasher, data.data(), data.size());
+
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
+}
+
+template <>
+void hashBlake3_keyed(const std::string& data, const std::string& key, std::vector<uint8_t>& out);
+
+template <typename Container>
+void hashBlake3_deriveKey(const Container& data, const std::string& context, std::vector<uint8_t>& out)
+{
+    blake3_hasher hasher;
+    blake3_hasher_init_derive_key(&hasher, context.data());
+    blake3_hasher_update(&hasher, data.data(), data.size());
+
+    if (out.empty() || out.size() < BLAKE3_OUT_LEN)
+        out.resize(BLAKE3_OUT_LEN);
+    blake3_hasher_finalize(&hasher, &out[0], BLAKE3_OUT_LEN);
+}
+
+template <>
+void hashBlake3_deriveKey(const std::string& data, const std::string& context, std::vector<uint8_t>& out);
+
 #endif    // BLAKE3_H
