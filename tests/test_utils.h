8113b641631303d5e24349da40cd5b03857ad51d
Waqar Ahmed <waqar.17a@gmail.com>
2020-08-17 20:17:56 +0500

    Fix warning converting from signed to unsigned

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -9,9 +9,10 @@ std::string HexStr(const T itbegin, const T itend)
     std::string rv;
     static const char hexmap[16] = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    rv.reserve(std::distance(itbegin, itend) * 2);
+    auto pos = static_cast<size_t>(std::distance(itbegin, itend));
+    rv.reserve(pos * 2ul);
     for (T it = itbegin; it < itend; ++it) {
-        unsigned char val = (unsigned char)(*it);
+        unsigned char val = static_cast<unsigned char>(*it);
         rv.push_back(hexmap[val >> 4]);
         rv.push_back(hexmap[val & 15]);
     }
